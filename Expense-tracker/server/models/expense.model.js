943ae9a7f1881c92c8491bcd24c54bd330cde253
wadeeeawwad <82997200+wadeeeawwad@users.noreply.github.com>
2021-08-10 13:10:27 +0300

    wadee change modle

---
+++ Expense-tracker/server/models/expense.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const TransactionSchema = new mongoose.Schema({
+  
+    name: {
+        type: String,
+        trim: true,
+        required: [true, 'Please add a name']
+    },
+    amount: {
+        type: Number,
+        required: [true, 'Please add a positive or negative number']
+    },
+    cheek:{
+        amount:{
+            type: Number,
+            required: [true, 'Please add a positive or negative number']
+
+        },
+        cheekDate:{
+            type: Date,
+            required: [true, 'Please add date .']          
+        }
+
+
+
+
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Transaction', TransactionSchema);
