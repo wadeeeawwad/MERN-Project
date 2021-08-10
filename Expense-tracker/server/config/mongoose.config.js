9277c21b098b7dc71cc667d52ec508f34afb7ba9
TareqHasa <mr.tareq.ha@gmail.com>
2021-08-10 16:34:39 +0300

    update models

--- Expense-tracker/server/config/mongoose.config.js
+++ Expense-tracker/server/config/mongoose.config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose
 	.connect("mongodb://localhost/beltDB", {
 		useNewUrlParser: true,
-		useCreateIndex: true,
+		// useCreateIndex: true,
 		useUnifiedTopology: true,
 	})
 	.then(() => console.log("Established a connection to the database"))
